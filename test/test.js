a67243fc736ae02ce9b17738ef74d9ec53b86066
Thomas Heller <thomas.heller@dynatrace.com>
2016-04-18 08:48:31 +0200

    update paths to testcases

--- test/test.js
+++ test/test.js
@@ -2,16 +2,15 @@ const assert = require('chai').assert;
 const linter = require('../index.js');
 
 describe('tags', function() {
+  describe('hooks', function() {
+    beforeEach(function() {
+      linter = require('../index.js');
+    });
+  });
 
   describe('sectiontag', function() {
-    describe('hooks', function() {
-      beforeEach(function() {
-        linter = require('../index.js');
-      });
-    });
-
     it('should return 2 Errors when section tag is unknown', function (done) {
-      linter.lintFile('./testcases/sectiontag-complete.md', function (err) {
+      linter.lintFile('./testcases/sectiontag/sectiontag-complete.md', function (err) {
         assert.equal(err.length, 2);
         assert.include(err[0].message, "Unknown tag \'section\'");
         assert.include(err[1].message, "Unknown tag \'endsection\'");
@@ -21,7 +20,7 @@ describe('tags', function() {
 
     it('should return 1 Error when section tag does not close', function (done) {
       linter.loadTags();
-      linter.lintFile('./testcases/sectiontag-incomplete.md', function (err) {
+      linter.lintFile('./testcases/sectiontag/sectiontag-incomplete.md', function (err) {
         assert.equal(err.length, 1);
         assert.include(err[0].message, "section tag was never closed");
         done();
@@ -30,7 +29,7 @@ describe('tags', function() {
 
     it('should return 1 Error when closed section tag does not open', function (done) {
       linter.loadTags();
-      linter.lintFile('./testcases/sectiontag-incomplete-02.md', function (err) {
+      linter.lintFile('./testcases/sectiontag/sectiontag-incomplete-02.md', function (err) {
         assert.equal(err.length, 1);
         assert.include(err[0].message, "Unknown tag \'endsection\'");
         done();
@@ -39,7 +38,7 @@ describe('tags', function() {
 
     it('should return no Error when bgimage parameter is good', function (done) {
       linter.loadTags();
-      linter.lintFile('./testcases/sectiontag-parameter-bgimage-passing.md', function (err) {
+      linter.lintFile('./testcases/sectiontag/sectiontag-parameter-bgimage-passing.md', function (err) {
         assert.equal(err.length, 0);
         done();
       });
@@ -47,10 +46,24 @@ describe('tags', function() {
 
     it('should return 1 Error when bgimage parameter is malformed', function (done) {
       linter.loadTags();
-      linter.lintFile('./testcases/sectiontag-parameter-bgimage.md', function (err) {
+      linter.lintFile('./testcases/sectiontag/sectiontag-parameter-bgimage.md', function (err) {
         assert.equal(err.length, 1);
         done();
-        // assert.include(err[0].message, "Unknown tag \'endsection\'");
+      });
+    });
+  });
+
+  describe('iftag', function() {
+    it('should return no Error when if tag is complete', function (done) {
+      linter.lintFile('./testcases/ifstatement/ifstatement-complete.md', function (err) {
+        assert.equal(err.length, 0);
+        done();
+      });
+    });
+    it('should return 1 Error when if tag is not closed', function (done) {
+      linter.lintFile('./testcases/ifstatement/ifstatement-incomplete.md', function (err) {
+        assert.equal(err.length, 1);
+        done();
       });
     });
   });
@@ -63,18 +76,33 @@ describe('variables', function() {
     });
   });
 
-  it('Should return an error, when variable is not closed', function (done) {
-    linter.lintFile('./testcases/variable-notclosing.md', function (err) {
+  it('Should return 1 Error, when variable is not closed', function (done) {
+    linter.lintFile('./testcases/variable/variable-notclosing.md', function (err) {
       assert.equal(err.length, 1);
       assert.include(err[0].message, "Variable \'{{\' was not properly terminated");
       done();
     });
   });
 
-  it('Should not return an error, when variable is not opening', function (done) {
-    linter.lintFile('./testcases/variable-notopening.md', function (err) {
+  it('Should return no Error, when variable is not opening', function (done) {
+    linter.lintFile('./testcases/variable/variable-notopening.md', function (err) {
       assert.equal(err.length, 0);
       done();
+    });
+  });
+});
+
+describe('fullpages', function() {
+  describe('hooks', function() {
+    beforeEach(function() {
+      linter = require('../index.js');
+      linter.loadTags();
+    });
+  });
+
+  it('Should return no Error when parsing well formed document', function (done) {
+    linter.lintFile('./testcases/fulldocuments/wellformed.md', function (err) {
+      asset.equal(err.length, 0);
     });
   });
 });
